56c069fa7bad29b41ba232d3cf7be630a4c5c527
Sidnei Baltazar Segatto Junior <157621521+SidneiBaltazarDev@users.noreply.github.com>
2024-09-25 13:15:52 -0300

    Crud Read
    CRUD.
    READ - DONE.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,13 +9,25 @@ class PostController extends Controller
 {
     public function create(Request $request) {
         $new_post = [
-            'title' => 'Titulo',
-            'content' => 'Conteudo Qualquer',
-            'author' => 'Sidnei'
+            'title' => 'Titulo 2 ',
+            'content' => 'Conteudo Qualquer 2',
+            'author' => 'Sidnei 2'
         ];
 
         $post = new Post($new_post);
         $post->save();
         dd($post);
+    }
+
+    public function read(Request $request) {
+        $post = new Post();
+
+        $post = $post->find(1);
+        return $post;
+    }
+
+    public function readall(Request $request) {
+        $posts = Post::all();
+        return $posts;
     }
 }
